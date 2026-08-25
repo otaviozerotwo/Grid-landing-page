af8a7b5f2f4e49ab54b7082db170aeead3f397d9
Otavio <otavio.zerotwo@gmail.com>
2026-08-24 21:17:44 -0300

    fix: import icons to toggleMenu function

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,14 @@
+import iconClose from '../assets/images/icon-close.svg'
+import iconMenu from '../assets/images/icon-menu.svg'
+
 const toggleMenu = (menu, icon, button) => {
   const isMenuOpen = menu.hidden;
   menu.hidden = !isMenuOpen;
   
-  const iconName = isMenuOpen ? 'icon-close' : 'icon-menu';
+  const iconPath = isMenuOpen ? iconClose : iconMenu;
   const ariaLabel = isMenuOpen ? 'Close menu' : 'Open menu';
 
-  icon.src = `./src/assets/images/${iconName}.svg`;
+  icon.src = iconPath;
   button.setAttribute('aria-label', ariaLabel);
   button.setAttribute('aria-expanded', String(isMenuOpen));
 }
